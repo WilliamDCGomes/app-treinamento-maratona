83ae1ac9050d8d0e0640db37e5354bdabaeff9d3
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-05-02 12:30:48 -0300

    Implementado splashscreen no android e ios, adicionado animação no menu inicial

--- MaratonaTreinamento/MaratonaTreinamento/Views/Login.xaml.cs
+++ MaratonaTreinamento/MaratonaTreinamento/Views/Login.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace MaratonaTreinamento.Views
@@ -10,6 +10,51 @@ namespace MaratonaTreinamento.Views
         public Login()
         {
             InitializeComponent();
+            buttonsAnimation();
+        }
+
+        async void buttonsAnimation()
+        {
+            if (DevicePlatform.Android == DeviceInfo.Platform)
+            {
+                versionText.TranslationY = -3000;
+                buttonCadastre.TranslationY = -3000;
+                buttonLogin.TranslationY = -3000;
+                frameApple.TranslationY = -3000;
+                frameFace.TranslationY = -3000;
+                frameGoogle.TranslationY = -3000;
+                secondText.TranslationY = -3000;
+                firstText.TranslationY = -3000;
+
+                await versionText.TranslateTo(0, 0, 150, Easing.BounceOut);
+                await buttonCadastre.TranslateTo(0, 0, 150, Easing.BounceOut);
+                await buttonLogin.TranslateTo(0, 0, 150, Easing.BounceOut);
+                await frameApple.TranslateTo(0, 0, 150, Easing.BounceOut);
+                await frameFace.TranslateTo(0, 0, 150, Easing.BounceOut);
+                await frameGoogle.TranslateTo(0, 0, 150, Easing.BounceOut);
+                await secondText.TranslateTo(0, 0, 150, Easing.BounceOut);
+                await firstText.TranslateTo(0, 0, 150, Easing.BounceOut);
+            }
+            else if (DevicePlatform.iOS == DeviceInfo.Platform)
+            {
+                versionText.TranslationX = 3000;
+                buttonCadastre.TranslationX = -3000;
+                buttonLogin.TranslationX = 3000;
+                frameApple.TranslationX = -3000;
+                frameFace.TranslationX = 3000;
+                frameGoogle.TranslationX = -3000;
+                secondText.TranslationX = 3000;
+                firstText.TranslationX = -3000;
+
+                await firstText.TranslateTo(0, 0, 150, Easing.BounceOut);
+                await secondText.TranslateTo(0, 0, 150, Easing.BounceOut);
+                await frameGoogle.TranslateTo(0, 0, 150, Easing.BounceOut);
+                await frameFace.TranslateTo(0, 0, 150, Easing.BounceOut);
+                await frameApple.TranslateTo(0, 0, 150, Easing.BounceOut);
+                await buttonLogin.TranslateTo(0, 0, 150, Easing.BounceOut);
+                await buttonCadastre.TranslateTo(0, 0, 150, Easing.BounceOut);
+                await versionText.TranslateTo(0, 0, 150, Easing.BounceOut);
+            }
         }
 
         async void SingInWithGoogle(System.Object sender, System.EventArgs e)
