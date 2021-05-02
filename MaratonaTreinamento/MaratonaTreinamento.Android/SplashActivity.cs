228644cb600017b71cca909b9c03b495aa7a06b3
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-05-02 00:44:54 -0300

    Inicializando layout de login

---
+++ MaratonaTreinamento/MaratonaTreinamento.Android/SplashActivity.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MaratonaTreinamento.Droid
+{
+    [Activity(Label = "MaratonaTreinamento", Theme = "@style/MainTheme.Splash", Icon = "@mipmap/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+            OverridePendingTransition(0, 0);
+        }
+    }
+}
---
+++ MaratonaTreinamento/MaratonaTreinamento/Views/Login.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MaratonaTreinamento.Views
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        async void SingInWithGoogle(System.Object sender, System.EventArgs e)
+        {
+            await frameGoogle.FadeTo(0.5, 100, Easing.BounceIn);
+            await frameGoogle.FadeTo(1, 100, Easing.BounceOut);
+        }
+
+        async void SingInWithFacebook(System.Object sender, System.EventArgs e)
+        {
+            await frameFace.FadeTo(0.5, 100, Easing.BounceIn);
+            await frameFace.FadeTo(1, 100, Easing.BounceOut);
+        }
+
+        async void SingInWithApple(System.Object sender, System.EventArgs e)
+        {
+            await frameApple.FadeTo(0.5, 100, Easing.BounceIn);
+            await frameApple.FadeTo(1, 100, Easing.BounceOut);
+        }
+
+        void EnterLogin(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        void NewUser(System.Object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
