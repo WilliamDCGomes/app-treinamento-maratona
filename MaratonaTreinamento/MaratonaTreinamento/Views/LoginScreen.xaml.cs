568c8ee4ca8f91a092627d531cf03a655711e4b2
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-05-05 23:33:00 -0300

    Começando a criar tela de acesso

--- MaratonaTreinamento/MaratonaTreinamento/Views/LoginScreen.xaml.cs
+++ MaratonaTreinamento/MaratonaTreinamento/Views/LoginScreen.xaml.cs
@@ -11,5 +11,33 @@ namespace MaratonaTreinamento.Views
         {
             InitializeComponent();
         }
+
+        void SingInWithGoogle(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void SingInWithFacebook(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void EnterLogin(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void NewUser(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void ShowAndHidePassword(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void DoForgetPassword(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void EnterMenu(System.Object sender, System.EventArgs e)
+        {
+        }
     }
 }
