ddd5ef0901df2edb8a67a251b1c0ee5da7cacaa1
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-05-15 14:56:28 -0300

    Começando a implementa novas mascaras de formatações

--- MaratonaTreinamento/MaratonaTreinamento/FormattingClass/MaskedBehavior.cs
+++ MaratonaTreinamento/MaratonaTreinamento/FormattingClass/MaskedBehavior.cs
@@ -7,6 +7,7 @@ namespace MaratonaTreinamento.FormattingClass
     public class MaskedBehavior : Behavior<Entry>
     {
         private string _mask = "";
+        private string _entry = null;
         public string Mask
         {
             get => _mask;
@@ -16,6 +17,8 @@ namespace MaratonaTreinamento.FormattingClass
                 SetPositions();
             }
         }
+        private bool getBack = false;
+        IDictionary<int, char> _positions;
 
         protected override void OnAttachedTo(Entry entry)
         {
@@ -29,7 +32,6 @@ namespace MaratonaTreinamento.FormattingClass
             base.OnDetachingFrom(entry);
         }
 
-        IDictionary<int, char> _positions;
 
         void SetPositions()
         {
@@ -40,38 +42,121 @@ namespace MaratonaTreinamento.FormattingClass
             }
 
             var list = new Dictionary<int, char>();
-            for (var i = 0; i < Mask.Length; i++)
-                if (Mask[i] != 'X')
-                    list.Add(i, Mask[i]);
-
+            var listAux = new List<string>();
+            if (string.IsNullOrEmpty(_entry) || _mask.Equals("XX/XX/XXXX"))
+            {
+                for (var i = 0; i < Mask.Length; i++)
+                    if (Mask[i] != 'X')
+                        list.Add(i, Mask[i]);
+            }
+            else if (string.IsNullOrEmpty(_entry) || _mask.Equals("XX:XX:XX"))
+            {
+                for (var i = 0; i < Mask.Length; i++)
+                    if (Mask[i] != 'X')
+                        list.Add(i, Mask[i]);
+            }
+            else if (string.IsNullOrEmpty(_entry) || _mask.Equals("XXX.XXX.XXX-XX"))
+            {
+                for (var i = 0; i < Mask.Length; i++)
+                    if (Mask[i] != 'X')
+                        list.Add(i, Mask[i]);
+            }
+            else if (string.IsNullOrEmpty(_entry) || _mask.Equals("XXXXX-XXX"))
+            {
+                for (var i = 0; i < Mask.Length; i++)
+                    if (Mask[i] != 'X')
+                        list.Add(i, Mask[i]);
+            }
+            else
+            {
+                bool jump = false;
+                int index = 0;
+                list.Clear();
+                listAux.Clear();
+                for (var i = 0; i < Mask.Length; i++)
+                {
+                    if (jump)
+                    {
+                        jump = false;
+                        continue;
+                    }
+                    else if (_entry.Trim().Length - 1 >= i && !string.IsNullOrEmpty(_entry.Substring(i, 1)))
+                    {
+                        if (_entry.Substring(i, 1).Equals("-") && _mask.Equals("(XX) XXXXX-XXXX"))
+                        {
+                            listAux.Add(_entry.Substring(i + 1, 1));
+                            listAux.Add("-");
+                            jump = true;
+                            continue;
+                        }
+                        else if (getBack && i == 9)
+                        {
+                            listAux.Add("-");
+                            listAux.Add(_entry.Substring(i, 1));
+                            jump = true;
+                        }
+                        else
+                        {
+                            listAux.Add(_entry.Substring(i, 1));
+                        }
+                    }
+                }
+                _entry = "";
+                foreach(string s in listAux)
+                {
+                    list.Add(index, char.Parse(s));
+                    _entry += s;
+                    index++;
+                }
+            }
             _positions = list;
         }
 
         private void OnEntryTextChanged(object sender, TextChangedEventArgs args)
         {
             var entry = sender as Entry;
-
-            var text = entry.Text;
+            _entry = entry.Text;
+            _entry?.Replace("-", "");
+            string text = entry.Text;
 
             if (string.IsNullOrWhiteSpace(text) || _positions == null)
                 return;
 
             if (text.Length > _mask.Length)
             {
-                entry.Text = text.Remove(text.Length - 1);
-                return;
-            }
-
-            foreach (var position in _positions)
-                if (text.Length >= position.Key + 1)
+                if (text.Length == 15 && _mask.Equals("(XX) XXXX-XXXX"))
                 {
-                    var value = position.Value.ToString();
-                    if (text.Substring(position.Key, 1) != value)
-                        text = text.Insert(position.Key, value);
+                    _mask = "(XX) XXXXX-XXXX";
+                    SetPositions();
+                    entry.Text = _entry;
                 }
+                else
+                {
+                    entry.Text = text.Remove(text.Length - 1);
+                    return;
+                }
+            }
+            else if (text.Length <= 14 && _mask.Equals("(XX) XXXXX-XXXX"))
+            {
+                _mask = "(XX) XXXX-XXXX";
+                getBack = true;
+                SetPositions();
+                entry.Text = _entry;
+                getBack = false;
+            }
+            else
+            {
+                foreach (var position in _positions)
+                    if (text.Length >= position.Key + 1)
+                    {
+                        var value = position.Value.ToString();
+                        if (text.Substring(position.Key, 1) != value)
+                            text = text.Insert(position.Key, value);
+                    }
 
-            if (entry.Text != text)
-                entry.Text = text;
+                if (entry.Text != text)
+                    entry.Text = text;
+            }
         }
     }
 }
---
+++ MaratonaTreinamento/MaratonaTreinamento/Views/MainMenu.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MaratonaTreinamento.Views
+{
+    public partial class MainMenu : ContentPage
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        void ListaNome_ItemSelected(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+        }
+    }
+}
